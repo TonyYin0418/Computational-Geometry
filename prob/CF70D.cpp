335b0f6580b9ef075a51ebbcd74ccb3b1176b58a
TonyYin0418 <TonyYin@88.com>
2021-11-28 09:04:28 +0800

    AC CF70D

--- prob/CF70D.cpp
+++ prob/CF70D.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 const int N = 1000;
 const double pi = 3.1415926535898;
 const double eps = 1e-10;
-inline int fcmp(double x, double y) { //x>y->1, x=y->0, x<y->-1
-	if(fabs(x - y) < eps) return 0;
-	else return x > y ? 1 : -1;
-}
 struct Point{
-	double x, y;
+	int x, y;
 	Point(){};
 	Point(double a, double b): x(a), y(b) {}
 	Point(Point a, Point b): x(b.x - a.x), y(b.y - a.y) {}
@@ -18,37 +15,38 @@ struct Point{
 	Point operator - (const Point &b) {
 		return Point(x - b.x, y - b.y);
 	}
-	double operator * (const Point &b) {
+	int operator * (const Point &b) {
 		return x * b.y - y * b.x;
 	}
-	double operator & (const Point &b) {
+	int operator & (const Point &b) {
 		return x * b.x + y * b.y;
 	}
 	bool operator == (const Point &b) {
-		return fcmp(x, b.x) == 0 && fcmp(y, b.y) == 0;
+		return x == b.x && y == b.y;
 	}
-	double len() {
+	int len() {
 		return sqrt(x * x + y * y);
 	}
 };
 map<int, int> top, down;
-bool check_top(int x, int y) {
+bool check_top(int x, int y) { //是否在上凸包下面
 	auto it = top.lower_bound(x);
-	 it = top.lower_bound(x);
 	if(it == top.end())
 		return false;
 	if(it -> first == x)
 		return y <= it->second;
+	if(it == top.begin()) return false;
 	auto jt = it; jt--;
 	return Point(x - it->first, y - it->second) *
 		   Point(x - jt->first, y - jt->second) >= 0;
 }
-bool check_down(int x, int y) {
+bool check_down(int x, int y) { //是否在下凸包上面
 	auto it = down.lower_bound(x);
 	if(it == down.end())
 		return false;
 	if(it -> first == x)
 		return y >= it->second;
+	if(it == down.begin()) return false;
 	auto jt = it; jt--;
 	return Point(x - it->first, y - it->second) *
 		   Point(x - jt->first, y - jt->second) <= 0;
@@ -106,10 +104,10 @@ void insert_down(int x, int y) {
 }
 
 int q;
-int main() {
-	scanf("%d", &q);
+signed main() {
+	scanf("%lld", &q);
 	while(q--) {
-		int opt, x, y; scanf("%d%d%d", &opt, &x, &y);
+		int opt, x, y; scanf("%lld%lld%lld", &opt, &x, &y);
 		if(opt == 1) {
 			insert_top(x, y);
 			insert_down(x, y);
